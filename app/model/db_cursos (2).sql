3ea8fd3b6894bef55f2eec916df1aa80002eefac
juan chantiri <juaniemilio05@gmail.com>
2025-10-20 17:11:01 -0300

    data base actualizada (user)

--- app/model/db_cursos (2).sql
+++ app/model/db_cursos (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-10-2025 a las 15:29:08
+-- Tiempo de generación: 20-10-2025 a las 22:09:57
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -38,7 +38,8 @@ CREATE TABLE `categoria` (
 
 INSERT INTO `categoria` (`id`, `nombre`) VALUES
 (1, 'Programacion'),
-(3, 'Marketing');
+(3, 'Marketing'),
+(5, 'Ingles');
 
 -- --------------------------------------------------------
 
@@ -63,7 +64,8 @@ INSERT INTO `curso` (`id`, `titulo`, `descripcion`, `instructor`, `imagen`, `id_
 (8, 'Java desde Cero: Fundamentos, Programación Orientada a Objetos y Desarrollo Backend', 'Este curso intensivo te guiará desde los conceptos básicos de Java hasta dominar la Programación Orientada a Objetos (POO), la base de la ingeniería de software moderna. Aprenderás a escribir código Java limpio, modular y reutilizable, preparándote para desarrollar aplicaciones robustas a nivel empresarial.', 'James Gosling', '68eabd3a8b6b0-cursoJava2.jpg', 1),
 (10, 'Estrategia de Marketing Digital 360°: De la Captación a la Conversión', 'Este curso te proporcionará una visión integral y práctica del Marketing Digital, enseñándote a diseñar, ejecutar y medir estrategias que realmente impulsan el crecimiento. Aprenderás a navegar el ecosistema digital, desde la creación de contenido hasta la optimización de embudos de venta, garantizando que cada inversión publicitaria genere un retorno real.', 'Lucas Britez', '68eabf154e244-marketing.jpg', 3),
 (11, 'Python para Data Science: De Cero a Primer Análisis de Datos con Pandas', 'Este curso te introduce al mundo de la programación Python con un enfoque práctico y directo en el Análisis de Datos. Aprenderás los fundamentos del lenguaje y, lo más importante, cómo utilizar las librerías líderes del sector como Pandas y NumPy para importar, limpiar, transformar y visualizar grandes volúmenes de datos. Al finalizar, estarás listo para enfrentar tus primeros proyectos de Data Science, convertir datos brutos en insights de negocio y construir reportes automáticos.', 'Guido van Rossum', '68ead919c8e09-python.jpg', 1),
-(12, 'Comunity managment', 'El curso de Community Management te prepara para gestionar, construir y fortalecer la presencia digital de una marca en redes sociales. Aprenderás a diseñar estrategias de comunicación efectivas, crear contenido atractivo, analizar métricas de desempeño y manejar comunidades online con una voz profesional y coherente.\r\n\r\nA lo largo del curso, conocerás las principales herramientas de gestión, programación y análisis de redes como Instagram, Facebook, TikTok y LinkedIn. Además, aprenderás buenas prácticas para gestionar crisis, aumentar el engagement y optimizar campañas publicitarias.\r\n\r\nIdeal para emprendedores, profesionales del marketing o cualquier persona interesada en posicionar marcas en el entorno digital.', 'Juan Martin del Potro', '68f2e37c5abfc-comunitimanager.jpg', 3);
+(12, 'Comunity managment', 'El curso de Community Management te prepara para gestionar, construir y fortalecer la presencia digital de una marca en redes sociales. Aprenderás a diseñar estrategias de comunicación efectivas, crear contenido atractivo, analizar métricas de desempeño y manejar comunidades online con una voz profesional y coherente.\r\n\r\nA lo largo del curso, conocerás las principales herramientas de gestión, programación y análisis de redes como Instagram, Facebook, TikTok y LinkedIn. Además, aprenderás buenas prácticas para gestionar crisis, aumentar el engagement y optimizar campañas publicitarias.\r\n\r\nIdeal para emprendedores, profesionales del marketing o cualquier persona interesada en posicionar marcas en el entorno digital.', 'Juan Martin del Potro', '68f2e37c5abfc-comunitimanager.jpg', 3),
+(15, '1er curso Ingles', 'Aprendé inglés de forma dinámica y práctica. Este curso está diseñado para ayudarte a desarrollar las cuatro habilidades fundamentales del idioma: comprensión auditiva, lectura, escritura y conversación. A través de clases interactivas, ejercicios comunicativos y situaciones reales, mejorarás tu pronunciación, ampliarás tu vocabulario y ganarás confianza para comunicarte en inglés tanto en contextos personales como profesionales.\r\n\r\nIdeal para estudiantes, viajeros o quienes buscan mejorar sus oportunidades laborales en un mundo cada vez más globalizado.', 'Pedro Pascal', '68f67e41db90f-ingles1ier.jpg', 5);
 
 -- --------------------------------------------------------
 
@@ -75,8 +77,16 @@ CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
   `email` varchar(250) NOT NULL,
   `password` varchar(250) NOT NULL,
-  `dni` int(11) NOT NULL
+  `es_admin` tinyint(1) DEFAULT 0,
+  `rol` varchar(20) DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `email`, `password`, `es_admin`, `rol`) VALUES
+(1, 'juaniemilio05@gmail.com', '$2y$10$DOXfZvNaVzNiBpTD/GdwW.H0xPCb0mkIVN/mHtfEb7O2GXluFw/Fi', 1, 'admin');
 
 --
 -- Índices para tablas volcadas
@@ -109,19 +119,19 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `curso`
 --
 ALTER TABLE `curso`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
